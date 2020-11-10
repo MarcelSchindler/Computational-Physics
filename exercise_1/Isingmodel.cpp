bc12667b9266c08db129a4fe8a6634d095e88323
Marcel Schindler <s6mcschi@uni-bonn.de>
2020-11-10 18:10:52 +0100

    plots T=1

--- exercise_1/Isingmodel.cpp
+++ exercise_1/Isingmodel.cpp
@@ -8,9 +8,9 @@
 
 using namespace std;
 
-double H(int s[], double h, int N)// Hamiltonian
+long double H(int s[], double h, int N)// Hamiltonian
 {
-    double value=0;
+    long double value=0;
        for (int i=0; i<N-1; i++)
 {
    value+=-s[i]*s[i+1]-h*s[i];
@@ -19,10 +19,10 @@ double H(int s[], double h, int N)// Hamiltonian
     return value;
 }
 
-double z(double h, int N, double T)//function for the partition function
+long double z(double h, int N, double T)//function for the partition function
 {
     int s[N];//Spin-Configuration-Array
-    double Z=0;
+    long double Z=0;
     for(int i=0;i<N;i++)//first we set all spins to s=-1
     {
         s[i]=-1;
@@ -43,19 +43,25 @@ double z(double h, int N, double T)//function for the partition function
     }
     return Z;
 }
-double z_analytic(double h, int N, double T)//analytic way to get Z
+long double z_analytic(double h, int N, double T)//analytic way to get Z
 {
-    double Z= pow(exp(1/T)*(cosh(h/T)+sqrt(sinh(h/T)*sinh(h/T)+exp(-4/T))),N);
+    long double Z= pow(exp(1/T)*(cosh(h/T)+sqrt(sinh(h/T)*sinh(h/T)+exp(-4/T))),N);
     Z+= pow(exp(1/T)*(cosh(h/T)+sqrt(sinh(h/T)*sinh(h/T)+exp(-4/T))),N);
     return Z;
 }
+long double z_analyticexact(double h, double T)//analytic way to get Z for N->00
+{ 
+    long double Z= exp(1/T)*cosh(h/T)+sqrt(exp(2/T)*sinh(h/T)*sinh(h/T)+exp(-2/T));
+    return Z;
+}
 //variables initialize
-double magnetization;
-double magnetization_analytic;
+long double magnetization;
+long double magnetization_analytic;
+long double magnetization_analytic_exact;
 double T;
 int N;
 double h;
-double Z;
+long double Z;
 
 
 
@@ -63,18 +69,20 @@ double Z;
 
 int main()
 {
-    T=300.0;//We let the Temperature T const= 300 
+    T=1;//We let the Temperature T const= 300 
     double delta_h = 10e-6;//this is the infintesimal for our derivativ
     fstream f;//this let us open/write in external Data
 
+   
     //N=const
     f.open("Nconst.dat", ios::out);//Open Nconst.dat
      N=20;//first we start with n=const
     for(int m=0;m<30;m++){//We do it for 30 different h for -1 to 1
     h=-1.0+2.0*m/29;
-    magnetization= 1000*T/N*((log(z(h+delta_h,N,T))-log(z(h-delta_h,N,T)))/(2*delta_h));//formular on the sheet with numerical derivativ. Added *1000 so the plots look nice
-    magnetization_analytic=1000*T/N*((log(z_analytic(h+delta_h,N,T))-log(z_analytic(h-delta_h,N,T)))/(2*delta_h));//analytic formular on the sheet
-    f << h <<' '<< magnetization <<' '<< magnetization_analytic << endl;//Write in Nconst.dat. endl makes a new line
+    magnetization= T/N*((log(z(h+delta_h,N,T))-log(z(h-delta_h,N,T)))/(2*delta_h));//formular on the sheet with numerical derivativ.
+    magnetization_analytic=T/N*((log(z_analytic(h+delta_h,N,T))-log(z_analytic(h-delta_h,N,T)))/(2*delta_h));//analytic formular on the sheet
+    magnetization_analytic_exact=T*((log(z_analyticexact(h+delta_h,T))-log(z_analyticexact(h-delta_h,T)))/(2*delta_h));//analytic formular on the sheet
+    f << h <<' '<< magnetization <<' '<< magnetization_analytic<< ' '<< magnetization_analytic_exact << endl;//Write in Nconst.dat. endl makes a new line
     }
     f.close();//we have to close Nconst.dat
 
@@ -83,8 +91,8 @@ int main()
     h=0.5;//h is no const
     for(int m=1;m<21;m++){// we do it vor N=1...20
     N=m;
-    magnetization= 1000*T/N*((log(z(h+delta_h,N,T))-log(z(h-delta_h,N,T)))/(2*delta_h));//formular on the sheet with numerical derivativ.
-    magnetization_analytic=1000*T/N*((log(z_analytic(h+delta_h,N,T))-log(z_analytic(h-delta_h,N,T)))/(2*delta_h));//analytic formular on the sheet
+    magnetization= T/N*((log(z(h+delta_h,N,T))-log(z(h-delta_h,N,T)))/(2*delta_h));//formular on the sheet with numerical derivativ.
+    magnetization_analytic=T/N*((log(z_analytic(h+delta_h,N,T))-log(z_analytic(h-delta_h,N,T)))/(2*delta_h));//analytic formular on the sheet
     f << N <<' '<< magnetization <<' '<< magnetization_analytic << endl;//write in hconst.dat
     }
     f.close();//close hconst
