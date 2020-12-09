b266ac78b875534b7613e2ea11ea1fa6146e7f38
Marcel Schindler <s6mcschi@uni-bonn.de>
2020-12-09 15:45:10 +0100

    new graphs. still wrong

--- exercise_5/Sheet5_Bhattarai_Schindler.cpp
+++ exercise_5/Sheet5_Bhattarai_Schindler.cpp
@@ -10,13 +10,18 @@
 using namespace std;
 
 
-double hamilton(double u[][3],double a, int N_max, int level)
+double hamilton(double u[][3], double phi[][3],double a_max, int N_max, int level)
 {
     double H_a=0;
     int N=N_max/pow(2,level);
+    double a=a_max*pow(2,level);
     for (int i = 0; i < N; i++)
     {
         H_a+=(u[i][level]-u[i-1][level])*(u[i][level]-u[i-1][level])/a;
+    }
+    for (int i = 0; i < N/2-1; i++)
+    {
+        H_a+=a*phi[i][level]*u[i][level];
     }
     return H_a;
 }
@@ -33,7 +38,7 @@ void fine_to_coarse(double u[][3], int N_max, int level)
 void coarse_to_fine(double u[][3], int N_max, int level)
 {
     int N=N_max/pow(2,level);
-    for (int i = 0; i < N; i++)
+    for (int i = 1; i < N-1; i++)
     {
         if (i %2==0)
         {
@@ -47,9 +52,10 @@ void coarse_to_fine(double u[][3], int N_max, int level)
     
 }
 
-double hamilton_coarse(double u[][3], double phi[][3], double a, int N_max, int level)
+double hamilton_coarse(double u[][3], double phi[][3], double a_max, int N_max, int level)
 {
     int N=N_max/pow(2,level);
+    double a=a_max*pow(2,level);
     double H_a=0;
     for (int i = 0; i < N/2; i++)
     {
@@ -62,9 +68,10 @@ double hamilton_coarse(double u[][3], double phi[][3], double a, int N_max, int 
     return H_a;
 }
 
-double magnitization(double u[][3], double a, int N_max, int level)
+double magnitization(double u[][3], double a_max, int N_max, int level)
 {
     int N=N_max/pow(2,level);
+    double a=a_max*pow(2,level);
     double magnitization=0;
     for (int i = 0; i < N-1; i++)
     {
@@ -72,9 +79,10 @@ double magnitization(double u[][3], double a, int N_max, int level)
     }
     return magnitization; 
 }
-double magnitization_square(double u[][3], double a, int N_max, int level)
+double magnitization_square(double u[][3], double a_max, int N_max, int level)
 {
     int N=N_max/pow(2,level);
+    double a=a_max*pow(2,level);
     double magnitization=0;
     for (int i = 0; i < N-1; i++)
     {
@@ -82,7 +90,7 @@ double magnitization_square(double u[][3], double a, int N_max, int level)
     }
     return magnitization; 
 }
-void sweep(double u[][3], double a_max, int N_max, int level, double delta)
+void sweep(double u[][3], double phi[][3],double a_max, int N_max, int level, double delta)
 {
     default_random_engine generator;// we need this to get an random number from a normal deistribution
     normal_distribution<double> distribution(0,1);
@@ -92,50 +100,57 @@ void sweep(double u[][3], double a_max, int N_max, int level, double delta)
     double random_number,H_start,H_end;
     int target_site;
     double u_changed;
+    int random_number2;
     for(int i = 0; i < N-1; i++)
     {
         random_number=((double)rand() / (double)RAND_MAX);
-        H_start=hamilton(u,a,N,level);
-        target_site=1+rand() % (N-2);
+        H_start=hamilton(u, phi, a,N,level);
+
+        target_site=1+(rand() % (N-2));
+
         u_changed=u[target_site][level];
         u[target_site][level]=u[target_site][level]+distribution(generator)*delta;
-        H_end=hamilton(u,a,N,level);
+        H_end=hamilton(u, phi ,a,N,level);
         if(random_number>exp(-(H_end-H_start)))
         {
             u[target_site][level]=u_changed;
         }
     }
 }
-void phi_calculation(double phi[][3], int N_max, int level)
+void phi_calculation(double phi[][3] ,double u[][3], double a ,int N_max, int level)
 {
     int N=N_max/pow(2,level);
-    for (int i = 0; i < N/2; i++)
+    phi[0][level+1]=(phi[N-1][level]+2*phi[0][level]+phi[1][level])/4;
+    phi[0][level+1]+=a*a*(u[N-1][level]-u[N-2][level]+u[0][level]-u[2][level]-u[N-3][level]+u[N-1][level]);
+    for (int i = 1; i < N/2; i++)
     {
-        phi[i][level+1]=(phi[2*i-1][level]+2*phi[2*i][level]+phi[2*i+1][level])/4;
+        phi[i][level+1]=(phi[2*i-1][level]+2*phi[2*i][level]+phi[(2*i+1 %N)][level])/4;
+        phi[i][level+1]+=a*a*(u[2*i-1][level]-u[2*i-2][level]+u[2*i][level]-u[(2*i+2 )%N][level]-u[2*i-3][level]+u[2*i-1][level]);
     }
-    
 }
-void multigrid(double u[][3], double phi[][3], double a, int N_max, int level, int sweep_lvl[], int gamma, int max_level, double delta)
+void multigrid(double u[][3], double phi[][3], double a, int N, int level, int sweep_lvl[], int gamma, int max_level, double delta)
 {
-    int N=N_max/pow(2,level);
-    if (level=! max_level)
-    {
     for (int i = 0; i < sweep_lvl[level]; i++)
     {
-        sweep(u,a,N,level,delta);
+        sweep(u, phi,a,N,level,delta);
     }
-    phi_calculation(phi,N,level);
-    fine_to_coarse(u,N,level);
-    for (int i = 0; i < gamma; i++)
+    if (level< max_level)
     {
-    u[64][level+1]={0};
-    multigrid(u, phi, a, N, level-1, sweep_lvl, gamma, max_level, delta);
-    }
-    coarse_to_fine(u,N,level);
+       
+        phi_calculation(phi,u, a,N,level);
+        for (int i = 0; i < gamma; i++)
+        {
+            for (int j = 0; j < 64; j++)
+            {
+                u[j][level+1]=0;
+            }
+        multigrid(u, phi, a, N, level+1, sweep_lvl, gamma, max_level, delta);
+        }
+        coarse_to_fine(u,N,level);
     }
     for (int i = 0; i < sweep_lvl[level]; i++)
     {
-        sweep(u,a,N,level,delta);
+        sweep(u,phi, a,N,level,delta);
     }
 }
 double  autocorrelation(double array[], double expected_value, int tau, int arraylenght, int startvalue)
@@ -166,29 +181,31 @@ int main()
     default_random_engine generator;// we need this to get an random number from a normal deistribution
     normal_distribution<double> distribution(0,1);
     double u[64][3]={0};
+    double phi[64][3]={0};
 
     fstream f;//this let us open/write in external Data
     f.open("Metropolis_Hasting.dat", ios::out);
     delta=2;
     N=64;
     L=64;
-    double magnitization_per_site[10000];
-    double magnitization_per_site_square[10000];
-    double energy_per_site[10000];
+    double magnitization_per_site[15000];
+    double magnitization_per_site_square[15000];
+    double energy_per_site[15000];
     double expactation_value;
-    double correlation_function[8000];
+    double correlation_function[13000];
+    
     a= L/N;
 
-    for (int j = 0; j < 10000; j++)
+    for (int j = 0; j < 15000; j++)
     {
     for (int i = 0; i < N-1; i++)
     {
         random_number=((double)rand() / (double)RAND_MAX);
-        H_start=hamilton(u,a,N,0);
-        target_site=1+rand() % (N-2);
+        H_start=hamilton(u,phi,a,N,0);
+        target_site=1+(rand() % (N-2));
         u_changed=u[target_site][0];
         u[target_site][0]=u[target_site][0]+distribution(generator)*delta;
-        H_end=hamilton(u,a,N,0);
+        H_end=hamilton(u,phi,a,N,0);
         if(random_number>exp(-(H_end-H_start)))
         {
             u[target_site][0]=u_changed;
@@ -196,10 +213,10 @@ int main()
     }
         magnitization_per_site[j]=magnitization(u,a,N,0);
         magnitization_per_site_square[j]=magnitization_square(u,a,N,0);
-        energy_per_site[j]= 1.0/N*hamilton(u,a,N,0);
+        energy_per_site[j]= 1.0/N*hamilton(u,phi,a,N,0);
     }
 
-    for (int i = 0; i < 10000; i++)
+    for (int i = 0; i < 15000; i++)
     {
          f<< i << ' '<< magnitization_per_site[i]<< ' '<< magnitization_per_site_square[i]<< ' '<< energy_per_site[i]<< endl;
     }
@@ -210,57 +227,71 @@ int main()
     f.open("Multigrid_gamma1.dat", ios::out);
     
     int gamma;
-    double phi[64][3]={0};
-    u[64][3]={0};
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 64; j++)
+        {
+            phi[j][i]=0;
+            u[j][i]=0;
+        }  
+    }
     int sweep_lvl[3]={4,2,1};
     int max_level=2;
     gamma=1;
     expactation_value=0;
-    for (int n = 0; n < 10000; n++)
+    for (int n = 0; n < 15000; n++)
     {
         multigrid(u,phi,a,N,0, sweep_lvl ,gamma,max_level, delta);
         magnitization_per_site_square[n]=magnitization_square(u,a,N,0);
     }
  
-    for (int i = 0; i < 8000; i++)
+    for (int i = 0; i < 13000; i++)
     {
-        expactation_value+=magnitization_per_site_square[i+2000];//calculate the error of phi
+        expactation_value+=magnitization_per_site_square[i+2000];
     }
-    expactation_value=expactation_value/8000;
-    for (int i = 0; i < 8000; i++)
+    expactation_value=expactation_value/13000;
+    for (int i = 0; i < 13000; i++)
     {
-        correlation_function[i]=autocorrelation(magnitization_per_site_square, expactation_value, i, 8000, 0);
+        correlation_function[i]=autocorrelation(magnitization_per_site_square, expactation_value, i, 15000, 0);
     }
 
-    for (int i = 0; i < 10000; i++)
+    for (int i = 0; i < 15000; i++)
     {
          f<< i << ' '<< magnitization_per_site_square[i]<< ' '<< correlation_function[i] << endl;
     }
     f.close();
 
 
-    f.open("Multigrid_gamma2.dat", ios::out);
-    phi[64][3]={0};
-    u[64][3]={0};
+    
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 64; j++)
+        {
+        phi[j][i]=0;
+        u[j][i]=0;
+        }
+        
+    }
     max_level=2;
     gamma=2;
     expactation_value=0;
-    for (int n = 0; n < 10000; n++)
+    for (int n = 0; n < 15000; n++)
     {
         multigrid(u,phi,a,N,0, sweep_lvl ,gamma,max_level, delta);
         magnitization_per_site_square[n]=magnitization_square(u,a,N,0);
+        
     }
-    for (int i = 0; i < 8000; i++)
+    for (int i = 0; i < 13000; i++)
     {
-        expactation_value+=magnitization_per_site_square[i+2000];//calculate the error of phi
+        expactation_value+=magnitization_per_site_square[i+2000];
     }
-    expactation_value=expactation_value/8000;
-    for (int i = 0; i < 8000; i++)
+    expactation_value=expactation_value/13000;
+    for (int i = 0; i < 13000; i++)
     {
-        correlation_function[i]=autocorrelation(magnitization_per_site_square, expactation_value, i, 8000, 0);
+        correlation_function[i]=autocorrelation(magnitization_per_site_square, expactation_value, i, 15000, 0);
     }
-
-    for (int i = 0; i < 10000; i++)
+    f.open("Multigrid_gamma2.dat", ios::out);
+    for (int i = 0; i < 15000; i++)
     {
          f<< i << ' '<< magnitization_per_site_square[i]<< ' '<< correlation_function[i] << endl;
     }
